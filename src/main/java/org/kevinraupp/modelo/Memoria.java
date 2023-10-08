4998fe44da6076946a300ffde179cb173cffec82
devkevin <rauppke@gmail.com>
2023-10-08 20:21:30 -0300

    .

--- src/main/java/org/kevinraupp/modelo/Memoria.java
+++ src/main/java/org/kevinraupp/modelo/Memoria.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
+    private enum TipoComando {
+            ZERAR, NUMERO,DIV,MULT,SUB,SOMA,IGUAL,VIRGULA;
+    };
     private static final Memoria instancia = new Memoria();
     private String textoAtual = "";
+    private String textoBuffer = "";
+    private boolean substituir = false;
+    private TipoComando ultimaOperacao = null;
     private final List<MemoriaObservador> observadores = new ArrayList<>();
     public Memoria(){
 
@@ -22,14 +28,50 @@ public class Memoria {
         observadores.add(o);
     }
 
-    public void processaroComando(String valor){
-        if("AC".equalsIgnoreCase(valor)){
-            textoAtual = "";
-        } else {
+    public void processaroComando(String texto){
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
 
-            textoAtual += valor;
+        if(tipoComando == null){
+            return;
+         } else if(tipoComando == TipoComando.ZERAR){
+            textoAtual = "";
+            textoBuffer = "";
+            substituir = false;
+            ultimaOperacao = null;
+        } else if(tipoComando == TipoComando.ZERAR || tipoComando == TipoComando.VIRGULA){
+            textoAtual = substituir ? texto : textoAtual + texto;
         }
+
+
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
 
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if(textoAtual.isEmpty() && texto == "0"){
+            return null;
+        }
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            if("AC".equalsIgnoreCase(texto)){
+                return TipoComando.ZERAR;
+            } else if("/".equalsIgnoreCase(texto)){
+                return TipoComando.DIV;
+            } else if("*".equalsIgnoreCase(texto)){
+                return TipoComando.MULT;
+            } else if("+".equalsIgnoreCase(texto)){
+                return TipoComando.SOMA;
+            } else if("-".equalsIgnoreCase(texto)){
+                return TipoComando.SUB;
+            } else if("=".equalsIgnoreCase(texto)){
+                return TipoComando.IGUAL;
+            } else if(",".equalsIgnoreCase(texto)){
+                return TipoComando.VIRGULA;
+            }
+        }
+        return null;
     }
 }
